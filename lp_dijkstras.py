2563d075d1296e85da323fa4a25dccfcbcd651cc
Daniel LaPrade <lapraddx@dukes.jmu.edu>
2024-04-30 13:36:59 -0400

    Longest Path Approx Done

--- lp_dijkstras.py
+++ lp_dijkstras.py
@@ -1,4 +1,5 @@
 import heapq
+from collections import deque
 
 
 def lp_dijkstras(G, s, dist, pred):
@@ -13,7 +14,9 @@ def lp_dijkstras(G, s, dist, pred):
                 dist[v] = dist[u] + G[u][v]
                 pred[v] = u
                 heapq.heappush(q, (dist[v], v))
-    return dist
+    # Compute Longest Path
+    min_value = min(dist.values())
+    return [(result, min_value) for result in dist if dist[result] == min_value]
 
 
 def main():
@@ -22,7 +25,7 @@ def main():
     n, m = input().split()
     for _ in range(int(m)):
         u, v, w = input().split()
-        w = float(w)
+        w = int(w)
         if u not in adj_list:
             adj_list[u] = {v: -w}
         else:
@@ -35,7 +38,18 @@ def main():
     # init sssp
     dist = {node: float('inf') for node in nodes}
     pred = {node: None for node in nodes}
-    print(lp_dijkstras(adj_list, 'a', dist, pred))
+    # Run algorithm on all nodes
+    maxpath = [(None, 0)]
+    stack = deque()
+    for node in nodes:
+        result = lp_dijkstras(adj_list, node, dist, pred)
+        # Add newest lp to the stack
+        if result[0][1] < maxpath[0][1]:
+            maxpath = result
+            stack.append(
+                f"{node} -> {maxpath[0][0]} (Length {-maxpath[0][1]})")
+    # Result
+    print(f"The Longest Path is {stack.pop()}")
 
 
 if __name__ == "__main__":
